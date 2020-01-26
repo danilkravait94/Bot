e190b82f1f3a661ee09b4b8ced6f5207180792a8
danilsdf <48866421+danilsdf@users.noreply.github.com>
2020-01-26 21:41:02 +0200

    1

---
+++ tekeg/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tekeg
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string ChatId { get; set; }
+        public string Name { get; set; }
+        public int DayCalories { get; set; }
+        public bool Male { get; set; }
+        public int Age { get; set; }
+        public float Weight { get; set; }
+        public float Height { get; set; }
+        public double Activity { get; set; }
+        public int Calories { get; set; }
+        public string Goal { get; set; }
+        public string Language { get; set; }
+        public string Command { get; set; }
+        public double BodyMassIndex { get; set; }
+
+    }
+}
---
+++ tekeg/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace tekeg
+{
+    class UserContext : DbContext
+    {
+        public UserContext()
+            : base("DbConnection")
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
